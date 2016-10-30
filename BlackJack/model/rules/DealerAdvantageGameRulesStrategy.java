5d55cbe2d35d610d37a3879409bd757df721b388
gk222gs <gk222gs@student.lnu.se>
2016-10-30 11:32:16 -0700

    DealerAdvantage done

--- BlackJack/model/rules/DealerAdvantageGameRulesStrategy.java
+++ BlackJack/model/rules/DealerAdvantageGameRulesStrategy.java
@@ -7,9 +7,13 @@ class DealerAdvantageGameRulesStrategy implements IGameRulesStrategy {
   public boolean isDealerWinner(Dealer a_dealer, Player a_player) {
     if (a_player.CalcScore() > a_dealer.GetMaxScore()) {
       return true;
-    } else if (a_player.CalcScore() > a_dealer.GetMaxScore()) {
+    } else if(a_dealer.CalcScore() > a_dealer.GetMaxScore()) {
+      return false;
+    } else if(a_player.CalcScore() < a_dealer.CalcScore() && a_dealer.CalcScore() <= a_dealer.GetMaxScore()) {
+      return true;
+    } else if (a_player.CalcScore() > a_dealer.CalcScore() && a_player.CalcScore() < a_dealer.GetMaxScore()) {
       return false;
     }
-    return a_player.CalcScore() >= a_dealer.CalcScore();
+    return true;
   }
 }
